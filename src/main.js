b3b2e6ef4595cbaccfbdd68fdb26f0110410c753
Andrey Kolesnikov <mailbox.kolesnikov@mail.ru>
2021-02-08 19:31:33 +0300

    Поставил element.io

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "element-ui/lib/theme-chalk/reset.css";
+import locale from "element-ui/lib/locale/lang/ru-RU";
+
+Vue.use(ElementUI);
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false;
 
